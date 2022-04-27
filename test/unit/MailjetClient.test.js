88fc19469fb9b0f3d7c83181aa7946fd5c2fb872
Denys <dzolo@softserveinc.com>
2022-04-27 19:34:05 +0300

    define unit test file structure; update eslint config file

---
+++ test/unit/MailjetClient.test.js
@@ -0,0 +1,112 @@
+/*external modules*/
+const chai = require('chai')
+/*other*/
+
+describe('Unit MailjetClient', () => {
+
+  describe('static part', () => {
+
+    describe('MailjetClient.constructor()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.connect()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+  })
+
+  describe('instance part', () => {
+
+    describe('MailjetClient.authStrategy()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.connectStrategy()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.isTokenRequired()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.typeJson()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.connect()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.setConfig()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.path()', () => {
+
+      it('pass', () => {
+        //
+      })
+
+    })
+
+    describe('MailjetClient.httpRequest()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.post()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.get()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.delete()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetClient.put()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+  })
+})
---
+++ test/unit/MailjetResource.test.js
@@ -0,0 +1,43 @@
+/*external modules*/
+const chai = require('chai')
+/*other*/
+
+describe('Unit MailjetResource', () => {
+
+  describe('static part', () => {
+
+    describe('MailjetResource.constructor()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+  })
+
+  describe('instance part', () => {
+
+    describe('MailjetResource.id()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetResource.action()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+    describe('MailjetResource.request()', () => {
+
+      it('pass', () => {
+      })
+
+    })
+
+  })
+
+})
