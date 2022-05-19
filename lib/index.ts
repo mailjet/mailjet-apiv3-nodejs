a077dc4ea881cd51c3345904c0a77ad2362e05bb
Denys <dzolo@softserveinc.com>
2022-05-19 21:30:29 +0300

    updated entry point of library to TypeScript

---
+++ lib/index.ts
@@ -0,0 +1,11 @@
+/*external modules*/
+/*utils*/
+/*types*/
+import HttpMethods from './request/HttpMethods';
+/*lib*/
+import Client from './client/index';
+import Request from './request/index';
+/*other*/
+
+export { Client, Request, HttpMethods };
+export default Client;
