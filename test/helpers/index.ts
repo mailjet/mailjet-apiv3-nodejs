6101130d3073bffcaeec5bd07a6a08b20b7c5c59
Denys <dzolo@softserveinc.com>
2022-05-19 21:31:08 +0300

    updated helpers for test to TypeScript

---
+++ test/helpers/index.ts
@@ -0,0 +1,14 @@
+/*external modules*/
+import { expect } from 'chai';
+/*types*/
+/*utils*/
+/*lib*/
+/*other*/
+
+function expectOwnProperty(targetObject: unknown, path: string, value: unknown) {
+  return expect(targetObject).to.have.ownProperty(path, value);
+}
+
+export {
+  expectOwnProperty,
+};
